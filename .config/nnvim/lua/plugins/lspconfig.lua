9c04cc6e86944e2377f33e6a6debbe12e4d673be
Abzsorb <Abzsorb@gmail.com>
2025-10-12 14:38:47 +0200

    Updated keybind

--- .config/nnvim/lua/plugins/lspconfig.lua
+++ .config/nnvim/lua/plugins/lspconfig.lua
@@ -17,7 +17,7 @@ return {
 				end
 				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
 				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-				map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gsr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 				map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 				map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
